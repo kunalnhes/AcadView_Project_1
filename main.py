f9b47b9c05ca12e21c760d159cb23f037af35542
kunalnhes <k.kunalj@outlook.com>
2017-08-03 12:04:09 +0530

    Input taken from the user and displayed

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print 'Let\'s get Startedd'
+print "Let's get started"
+spy_name = raw_input("Please enter your name: ")
+salutation = raw_input("Please enter your salutation: ")
+#Concatenation of salutation with the name.
+spy_name = salutation+" "+spy_name
+print "Welcome "+spy_name;
