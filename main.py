e9e42e763539ff4e6bad82313ddb56ad872e5ccd
kunalnhes <k.kunalj@outlook.com>
2017-08-03 12:29:04 +0530

    Condition statement done

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 print "Let's get started"
 spy_name = raw_input("Please enter your name: ")
-salutation = raw_input("Please enter your salutation: ")
-#Concatenation of salutation with the name.
-spy_name = salutation+" "+spy_name
-print "Welcome "+spy_name;
+#Concatenation of salutation with the name and condition checking.
+if len(spy_name)>0:
+    salutation = raw_input("Please enter your salutation: ")
+
+    if len(salutation)>0:
+        spy_name = salutation + " " + spy_name
+        print "Welcome "+spy_name;
+    else:
+        print "You did not entered your salutation. Please retry!"
+
+else:
+    print "You did not entered your name. Please retry!"
